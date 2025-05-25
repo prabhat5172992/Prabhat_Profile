1d6591a8e127ecbc09f428ba83128539884c1100
Prabhat Ranjan <prabhat5172992@gmail.com>
2025-05-26 00:07:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Prabhat_Profile/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
