a27384390a8224d61d43be11727b45b5c71c7d08
Prabhat Ranjan <prabhat5172992@gmail.com>
2025-05-25 22:12:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'http://prabhat5172992.github.io/Prabhat_Profile/',
 })
